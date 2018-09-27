893e3201c23e6b20b515b64dc4ba29973947881e
qarkgwanjun <42165150+qarkgwanjun@users.noreply.github.com>
2018-09-27 23:43:02 +0900

    .

---
+++ CatchMole2/main.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+#include <stdlib.h>
+#include <Windows.h>
+#include <Time.h>
+#include <conio.h>
+#include <cstdlib>
+
+#define TRUE 1
+
+using namespace std;
+void StartMoleGame();
+void gotoxy(int x, int y);
+void setcolor(int color, int bgcolor);
+void KeepHole();
+int MoveMole(int *Location);
+int* Location();
+int CatchMole(int *a);
+int PrintResult(int *score, int *count, int *last);
+
+
+
